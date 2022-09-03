14047360ee32373c7f55722a24a901f35cc27192
yurun <>
2022-09-03 16:12:04 +0800

    Constant 优化常量注释

--- src/main/java/cn/datastacks/durian/Constant.java
+++ src/main/java/cn/datastacks/durian/Constant.java
@@ -6,64 +6,165 @@ package cn.datastacks.durian;
  * @author yurun
  */
 public class Constant {
+    /**
+     * &
+     */
     public static final String AMPERSAND = "&";
 
+    /**
+     * =
+     */
     public static final String EQUAL = "=";
 
+    /**
+     * |
+     */
     public static final String VERTICAL_BAR = "|";
 
+    /**
+     * ;
+     */
     public static final String SEMICOLON = ";";
 
+    /**
+     * 空格
+     */
     public static final String SPACE = " ";
 
+    /**
+     * ,
+     */
     public static final String COMMA = ",";
 
+    /**
+     * .
+     */
     public static final String FULL_STOP = ".";
 
+    /**
+     * _
+     */
     public static final String UNDERLINE = "_";
 
+    /**
+     * /
+     */
     public static final String SLASH = "/";
 
+    /**
+     * :
+     */
     public static final String COLON = ":";
+    /**
+     * ::
+     */
     public static final String DOUBLE_COLON = "::";
 
+    /**
+     * \n
+     */
     public static final String NEWLINE = "\n";
 
+    /**
+     * *
+     */
     public static final String ASTERISK = "*";
 
+    /**
+     * 空白
+     */
     public static final String EMPTY = "";
 
+    /**
+     * -
+     */
     public static final String DASH = "-";
 
+    /**
+     * (
+     */
     public static final String LEFT_PARENTHESIS = "(";
+    /**
+     * )
+     */
     public static final String RIGHT_PARENTHESIS = ")";
 
+    /**
+     * [
+     */
     public static final String LEFT_BRACKET = "[";
+    /**
+     * ]
+     */
     public static final String RIGHT_BRACKET = "]";
 
+    /**
+     * {
+     */
     public static final String LEFT_BRACE = "{";
+    /**
+     * }
+     */
     public static final String RIGHT_BRACE = "}";
 
+    /**
+     * -
+     */
     public static final String LINE_THROUGH = "-";
 
+    /**
+     * 1
+     */
     public static final int POSITIVE_ONE = 1;
+    /**
+     * -1
+     */
     public static final int NEGATIVE_ONE = -1;
+    /**
+     * 0
+     */
     public static final int ZERO = 0;
 
+    /**
+     * 空整数数组
+     */
     public static final int[] INT_EMPTY_ARRAY = new int[0];
+    /**
+     * 空长整数数组
+     */
     public static final long[] LONG_EMPTY_ARRAY = new long[0];
+    /**
+     * 空单精度浮点数数组
+     */
     public static final float[] FLOAT_EMPTY_ARRAY = new float[0];
+    /**
+     * 空双精度浮点数数组
+     */
     public static final double[] DOUBLE_EMPTY_ARRAY = new double[0];
+    /**
+     * 空布尔数组
+     */
     public static final boolean[] BOOLEAN_EMPTY_ARRAY = new boolean[0];
+    /**
+     * 空字符串数组
+     */
     public static final String[] STRING_EMPTY_ARRAY = new String[0];
 
-    public static final String UNKNOWN = "unknown";
-
+    /**
+     * 年-月-日
+     */
     public static final String LOCAL_DATE = "yyyy-MM-dd";
+    /**
+     * 年-月-日 时:分:秒
+     */
     public static final String LOCAL_DATE_TIME = "yyyy-MM-dd HH:mm:ss";
-    public static final String LOCAL_DATE_TIME_START = "yyyy-MM-dd 00:00:00";
-    public static final String LOCAL_DATE_TIME_END = "yyyy-MM-dd 23:59:59";
 
+    /**
+     * http
+     */
     public static final String HTTP = "http";
+    /**
+     * https
+     */
     public static final String HTTPS = "https";
 }
